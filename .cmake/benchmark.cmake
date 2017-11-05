8e6e0ba47ed9af9c3deb4b90c48f5ca93457ca2c
Lenny Maiorani <ldm5180@gmail.com>
2017-11-05 02:08:15 +0000

    :new: Google Benchmark support
    Problem:
    - Google Benchmark library is not built-in, but projects often want it.
    
    Solution:
    - Add support for quickly adding some benchmarks.

---
+++ .cmake/benchmark.cmake
@@ -0,0 +1,3 @@
+option(BENCHMARK_ENABLE_TESTING "Enable testing of the benchmark library." OFF)
+option(BENCHMARK_ENABLE_INSTALL "Enable installation of benchmark." OFF)
+add_subdirectory(vendor/benchmark)
